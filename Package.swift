674eb8fc1658a1dd129a836e13ba2c2b8d6e10f3
Anders Evjen <evjand@gmail.com>
2025-04-09 11:02:52 +0200

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -21,7 +21,7 @@ let package = Package(
         ),
     ],
     dependencies: [
-        .package(url: "https://github.com/swiftlang/swift-syntax.git", "509.0.0"..<"601.0.0-prerelease"),
+        .package(url: "https://github.com/swiftlang/swift-syntax", "509.0.0"..<"602.0.0"),
     ],
     targets: [
         // Macro implementation that performs the source transformation of a macro.
